dc1582e01c7e66ec154e6d4d0b93c9ed128464fd
ReflexiveComputing <122396666+ReflexiveComputing@users.noreply.github.com>
2025-08-10 01:36:10 +0200

    modifying static menu file and type

--- src/types/menu.ts
+++ src/types/menu.ts
@@ -1,14 +1,16 @@
-type MenuItem = {
+export type MenuItem = {
   id: number;
   name: string;
   description: string;
   longDescription: string;
   price: string;
+  image: string;
   images: string[];
   likes: number;
   isVegetarian: boolean;
   badge: string;
-  badgeColor: "gold" | "green" | "purple" | "default" | null | undefined
+  badgeColor: "gold" | "green" | "purple" | "default" | null | undefined;
   tags: string[];
-  image?: string; // Optional for backward compatibility
+  category: string[]; // NEW
+  mainCategory: string; // NEW
 };
