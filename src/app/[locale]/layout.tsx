5d5a84ccafad652d037676418ae3818f869c41a2
ReflexiveComputing <122396666+ReflexiveComputing@users.noreply.github.com>
2025-09-19 21:00:03 +0200

    adding analytics

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
 import { Geist, Geist_Mono } from "next/font/google";
 import React from "react";
 import { NextIntlClientProvider } from 'next-intl';
