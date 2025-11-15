5deb6d04516c2be8809f28352a0e71c3567de40f
ReflexiveComputing <122396666+ReflexiveComputing@users.noreply.github.com>
2025-11-15 15:51:08 +0100

    updating whishlist item layout

--- src/components/ui/food-list.tsx
+++ src/components/ui/food-list.tsx
@@ -21,7 +21,7 @@ export default function FoodListItem({ favorite }: { favorite: WishlistItem }) {
 
                 <div className="w-2/3 flex-1 flex-start">
                     <div className="flex items-center flex-start gap-2">
-                        <h3 className="text-lg text-app-light-highlight font-medium">{favorite.menuItem.name}</h3>
+                        <h3 className="px-1 text-lg text-app-light-highlight font-medium">{favorite.menuItem.name}</h3>
                         <div className="flex gap-2 self-end">
                             {favorite.menuItem.mainIngredients?.map(mi =>
                                 <Icon key={mi.ingredient?.id} className="text-app-light-highlight w-3 h-3" icon={mi.ingredient?.iconName || "mdi:information-outline"} />
@@ -37,9 +37,9 @@ export default function FoodListItem({ favorite }: { favorite: WishlistItem }) {
                 </div>
             </div>
             <div className="flex w-full justify-start min-h-[60px]">
-                <VerticalDashedLines className="mx-2 max-w-0.5 min-h-full" color="#febd3a" dashHeight={4} gap={3} width={2} />
+                <VerticalDashedLines className="mx-3 max-w-0.5 min-h-full" color="#febd3a" dashHeight={4} gap={3} width={2} />
                 {favorite.menuItem.shortDescription ? (
-                    <p className="w-full description text-sm text-gray-200">{favorite.menuItem.shortDescription}</p>
+                    <p className="w-4/7 px-2 description text-sm text-gray-200">{favorite.menuItem.shortDescription}</p>
                 ) : <p className="w-3/5 description text-sm text-gray-200">Some text to test the component some more text</p>}
             </div>
         </div>
