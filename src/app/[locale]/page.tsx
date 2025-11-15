a3f8497cd1890fd7bf0901520b7df1d0815bd517
ReflexiveComputing <122396666+ReflexiveComputing@users.noreply.github.com>
2025-11-15 16:06:55 +0100

    front page update

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,6 +6,7 @@ import Image from "next/image"
 import { Link } from '@/i18n/routing';
 import { useTranslations } from 'next-intl';
 import { useNavbar } from "@/components/providers/navbar-provider";
+import NepaliSunIcon from "@/components/ui/icons/svg/nepali-sun";
 
 export default function HomePage() {
   const t = useTranslations('HomePage');
@@ -30,28 +31,32 @@ export default function HomePage() {
         {/* Main Content */}
         <div className="flex-1 flex flex-col items-center justify-center px-6 py-1">
           {/* Logo */}
+          <div className="flex flex-col justify-center text-center uppercase font-[family-name:var(--font-fjalla-one)] ">
+            <div className="m-auto w-fit">
+              <NepaliSunIcon size={64} backgroundColor="#febd3a"/>
+            </div>
+            <h1 className="text-5xl font-medium text-white mb-2 tracking-tight drop-shadow-lg ">{t('title')}</h1>
+          </div>
           <div className="">
             <Image
               src="/Mithocha_logo_transparent.png"
               alt="Mithocha Logo"
-              width={200}
-              height={100}
+              width={270}
+              height={200}
               className="object-contain"
               priority
             />
           </div>
 
           {/* Headline */}
-          <div className="text-center uppercase ">
-            <h1 className="text-6xl font-medium text-white mb-2 tracking-tight drop-shadow-lg font-[family-name:var(--font-fjalla-one)]">{t('title')}</h1>
-            <h1 className="text-5xl font-medium text-white mb-2 tracking-tight drop-shadow-lg font-[family-name:var(--font-fjalla-one)]">{t('subtitle')}</h1>
-            <h1 className="text-4xl font-medium text-white tracking-tight drop-shadow-lg font-[family-name:var(--font-fjalla-one)]">{t('subtitle2')}</h1>
+          <div className=" flex flex-col text-center uppercase font-[family-name:var(--font-fjalla-one)] ">
+            <h1 className="text-2xl font-medium text-white mb-2 tracking-tight drop-shadow-lg ">{t('subtitle')}</h1>
           </div>
         </div>
 
         {/* Action Buttons - Positioned at bottom */}
         <div className="px-6 pt-20">
-          <div className="w-full max-w-sm mx-auto space-y-4 ">
+          <div className="w-2/3 max-w-sm mx-auto space-y-4 ">
             <Button variant="surpriseMe" asChild>
               <Link href="/food" className="w-block font-[family-name:var(--font-fjalla-one)]" onClick={() => setSelectedNav('food')}>
                 {t('helpMeChoose')}
