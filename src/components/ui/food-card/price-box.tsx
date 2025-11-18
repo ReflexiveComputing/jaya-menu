11f1c08d02746533cab4090744269fe2a30721c0
ReflexiveComputing <122396666+ReflexiveComputing@users.noreply.github.com>
2025-11-18 08:12:05 +0100

    fix pricebox size

--- src/components/ui/food-card/price-box.tsx
+++ src/components/ui/food-card/price-box.tsx
@@ -11,7 +11,7 @@ export function PriceBox({ price, className = "" }: PriceBoxProps) {
   const [priceMain, priceDec] = priceStr.split(".");
 
   return (
-    <div className={`absolute top-0 left-0 py-2 w-11 h-8 transition-colors bg-app-dark-highlight ${className}`}>
+    <div className={`absolute top-0 left-0 p-1 min-w-11 min-h-8 transition-colors bg-app-dark-highlight ${className}`}>
       <div className="flex items-baseline m-auto">
         <span className="font-medium text-sm text-app-light-highlight">€</span>
         <span className="font-medium ml-0.5 text-sm leading-none text-gray-200">{priceMain}</span>
