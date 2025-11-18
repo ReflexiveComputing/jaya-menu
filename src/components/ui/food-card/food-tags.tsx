e0245993f55c0d72deb4828d43a7a29b49650acb
ReflexiveComputing <122396666+ReflexiveComputing@users.noreply.github.com>
2025-11-18 09:09:17 +0100

    icon loading fix

--- src/components/ui/food-card/food-tags.tsx
+++ src/components/ui/food-card/food-tags.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import * as React from "react"
 import { DynamicIcon, IconName } from "lucide-react/dynamic"
 import { Icon } from '@iconify-icon/react';
@@ -15,12 +17,33 @@ export function FoodTags({
     className,
     color,
 }: FoodTagsProps) {
-    const text = `text-[${color || '#E4C4AE'}]`
+    const [mounted, setMounted] = React.useState(false)
+    const iconColor = color || '#E4C4AE'
+    
+    React.useEffect(() => {
+        setMounted(true)
+    }, [])
+    
+    if (!mounted) {
+        return (
+            <div className="m-auto mt-1 pr-2 items-center justify-center flex flex-col gap-2">
+                {tags.map((tag, idx) => (
+                    <div key={`${tag}-${idx}`} className="flex w-5 h-5" />
+                ))}
+            </div>
+        )
+    }
+    
     return (
-        <div className={`m-auto mt-1 pr-2 items-center justify-center flex flex-col gap-2 ${text}`}>
+        <div className="m-auto mt-1 pr-2 items-center justify-center flex flex-col gap-2">
             {tags.map((tag, idx) => (
-                <div key={tag + idx} className={`flex w-5 h-5`}>
-                    <Icon className={"w-5 h-5"} icon={tag} />
+                <div key={`${tag}-${idx}`} className="flex w-5 h-5">
+                    <Icon 
+                        className="w-5 h-5" 
+                        icon={tag}
+                        style={{ color: iconColor }}
+                        mode="svg"
+                    />
                 </div>
             ))}
         </div>
